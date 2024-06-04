3202acc30b9fd419b1aac8643c2af31084efdbe6
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-06-04 11:53:23 +0530

    object

--- forname.js
+++ forname.js
@@ -2,3 +2,5 @@ let enames =["muni,mani,priya,jashu,mahitha"]
 for(ename  of enames){
     console.log(ename)
 }
+
+delete muni;
---
+++ object.js
@@ -0,0 +1,17 @@
+let a = [0]
+let emp = {"id":101 , "name": "Rahul","salary":45000}
+
+
+console.log(emp.id)
+console.log(emp.name)
+console.log(emp.salary)
+console.log(emp.loc)
+
+
+emp.name = "rahulgandhi"  
+console.log(emp)
+
+delete emp.salary
+console.log(emp)
+
+
