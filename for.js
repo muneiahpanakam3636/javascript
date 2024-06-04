b9982908aa26e000c792d5612bfa27e9d04835ef
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-06-04 11:10:32 +0530

    tst

---
+++ for.js
@@ -0,0 +1,4 @@
+let eids = [101,102,103,104,105]
+for(eid of eids){
+    console.log(eid)
+}
---
+++ forname.js
@@ -0,0 +1,4 @@
+let enames =["muni,mani,priya,jashu,mahitha"]
+for(ename  of enames){
+    console.log(ename)
+}
