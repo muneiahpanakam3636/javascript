21da6fa11f53addcc2c5ff7841603a9d389165e5
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-06-06 12:12:44 +0530

    tst

---
+++ employeesfor.js
@@ -0,0 +1,9 @@
+let employees = [{id:101,name:"rahul",sal:45000},
+                 {id :102,name:"Sonia",sal:55000},
+                 {id:103,name:"priya",sal:65000}
+]
+for(i=0;  i<=2;  i++){
+    console.log(employees[i].id)
+    console.log(employees[i].name)
+    console.log(employees[i].sal)
+}
