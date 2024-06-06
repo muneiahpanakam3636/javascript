5ff6a5e13b6a921c51c1b06cf9dd50cc2ec5d674
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-06-06 12:09:34 +0530

    tst

---
+++ employeeswhile.js
@@ -0,0 +1,11 @@
+let employees = [{id:101,name:"rahul",sal:45000},
+                 {id :102,name:"Sonia",sal:55000},
+                 {id:103,name:"priya",sal:65000}
+]
+let i =0;
+while(i<=2){
+    console.log(employees[i].id)
+    console.log(employees[i].name)
+    console.log(employees[i].sal)
+    i++
+}
