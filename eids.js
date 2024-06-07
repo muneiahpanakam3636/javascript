6ecff6bf20137eb003458d8af86b30d9d0ac140c
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-06-07 16:09:41 +0530

    tst

---
+++ eids.js
@@ -0,0 +1,25 @@
+let eids = [101,102,103,104]
+
+let new_len = eids.push(105)
+console.log(eids)
+
+
+eids.unshift(100)
+console.log(eids)
+
+
+
+eids.pop()
+console.log(eids)
+
+
+eids.shift()
+console.log(eids)
+
+eids.length
+console.log(eids)
+
+eids.reverse()
+console.log(eids)
+
+
---
+++ slice.js
@@ -0,0 +1,4 @@
+let eids = [101,102,103,104]
+
+eids.slice()
+console.log(eids)
