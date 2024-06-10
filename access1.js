fca181af2c23baa8108dd63f7879422d74ddf664
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-06-10 15:59:19 +0530

    tst

---
+++ access1.js
@@ -0,0 +1,4 @@
+let enames = ["muni","saru"]
+
+let index = enames[1]
+console.log(index);
---
+++ accesstheitems.js
@@ -0,0 +1,9 @@
+const fruits = ['apple', 'banana', 'orange', 'grape'];
+
+// Access the first element
+const firstFruit = fruits[0];
+console.log(firstFruit); // Output: "apple"
+
+// Access the third element
+const thirdFruit = fruits[2];
+console.log(thirdFruit); // Output: "orange"
