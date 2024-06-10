f28dd916da7034c32163809cd1760687fcd88a89
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-06-10 11:38:37 +0530

    tst

---
+++ createobject.js
@@ -0,0 +1,11 @@
+let emp={
+    eid:101,
+    ename:"Rahul",
+    esal:55000
+}
+let user={}
+
+console.log(Object.values(emp))
+console.log(Object.values(emp).length)
+console.log(Object.values(emp).length>0)
+
