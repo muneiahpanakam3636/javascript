4d65678e2b33832d08c9519d6fc35780cc3d69b2
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-06-10 14:55:09 +0530

    tst

--- emptyornot.js
+++ emptyornot.js
@@ -1,5 +1,6 @@
 let employees = [
-    
+    {},
+    {}
 ]
 let array= []
 
---
+++ objectkey.js
@@ -0,0 +1,9 @@
+let emp={
+    eid:101,
+    ename:"Rahul",
+    esal:55000
+}
+console.log(emp.eid) 
+console.log(emp.ename) 
+console.log(emp.esal) 
+console.log(emp.legth)
