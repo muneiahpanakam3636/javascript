be5e77b0592cd300f2c268d8b1303f9df8fd517e
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-06-29 10:50:37 +0530

    test

---
+++ ASYNCHRONOUS/three1callback.js
@@ -0,0 +1,15 @@
+let  sum = (a,b)=>{
+    return a+b
+}
+let   multi =(a,b)=>{
+    return a*b
+}
+let cal = (a,b,callback)=>{
+    return callback(a,b)
+}
+
+let r1 = cal(10,20,sum)
+console.log(r1);
+let r2 = cal(10,20,multi)
+console.log(r2);
+
